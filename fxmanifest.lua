ce237c0ee1a8a0237162ad816fd92ee2f9ae1a94
SHOCK <269146576+ShocksDevelopment@users.noreply.github.com>
2026-03-18 12:45:39 +0000

    Headshot Removal
    Remove insta headshot kills

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Tony Avacardo'
+name 'Stop Headshot Damage'
+version '1.0.0'
+
+client_scripts {
+	'client.lua'
+}
+client_script "@Anticheat/acloader.lua"
